8af96880f4e55d6aa7be8d6ffd3e137f6425f847
Eric Yang <ericyang321@gmail.com>
2020-07-03 14:09:40 -0700

    add merge sort implementation add comparator for merge sort

--- python/sorts/sorts.py
+++ python/sorts/sorts.py
@@ -15,5 +15,59 @@ def selection_sort(l):
 def insertion_sort(l):
     pass
 
-def merge_sort(l):
-    pass
+
+################# merge sort ##################
+
+
+def default_comparator(a, b):
+    return a - b
+
+
+def merge_sorted_lists(a_list, b_list, comparator=default_comparator):
+    A_LEN = len(a_list)
+    B_LEN = len(b_list)
+
+    a_idx = 0
+    b_idx = 0
+
+    combined_list = []
+
+    while a_idx < A_LEN and b_idx < B_LEN:
+        a_item = a_list[a_idx]
+        b_item = b_list[b_idx]
+
+        pre_order_result = comparator(a_item, b_item)
+        post_order_result = comparator(b_item, a_item)
+
+        if pre_order_result < post_order_result:
+            combined_list.append(a_item)
+            a_idx += 1
+        else:
+            combined_list.append(b_item)
+            b_idx += 1
+
+    # concat any other list's left over items when
+    # one list has its items exhausted
+    if a_idx < A_LEN:
+        for idx in range(a_idx, A_LEN):
+            combined_list.append(a_list[idx])
+    elif b_idx < B_LEN:
+        for idx in range(b_idx, B_LEN):
+            combined_list.append(b_list[idx])
+
+    return combined_list
+
+
+def merge_sort(sortable, comparator=default_comparator):
+    if len(sortable) <= 1:
+        return sortable
+
+    half_idx = len(sortable) // 2
+    left_half = sortable[:half_idx]
+    right_half = sortable[half_idx:]
+
+    # recurse
+    sorted_left_half = merge_sort(left_half, comparator)
+    sorted_right_half = merge_sort(right_half, comparator)
+
+    return merge_sorted_lists(sorted_left_half, sorted_right_half, comparator)
