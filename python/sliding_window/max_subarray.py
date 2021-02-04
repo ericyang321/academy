b523f91e32709beee4eda76d6144f6e7155d050f
Eric Yang <ericyang321@gmail.com>
2021-02-03 21:19:50 -0800

    add sliding window max subarray implementation

---
+++ python/sliding_window/max_subarray.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+def max_subarray(nums, k):
+    """
+    find max possible summed subarray value of k elements. k is 1 indexed
+    """
+    max_num = float("-inf")
+    current_num = 0
+    for idx, num in enumerate(nums):
+        current_num += num
+        if idx >= k:
+            current_num -= nums[idx - k]
+            max_num = max(max_num, current_num)
+
+    return max_num
